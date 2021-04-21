992dea20f754333df51a5f9553b1f7bd12102813
tnguyen0306 <tnguyen0306@gmail.com>
2021-04-21 17:14:29 -0500

    Beta 1.1.0 - add /scrape

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape
+import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-
-
-mongo = PyMongo(app, uri="mongodb://localhost:27017/app_name")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
     # # Find one record of data from the mongo database
-    data={} 
+    data = mongo.db.collection.find_one() 
 
     # # Return template and data
-    return render_template("index.html", data=data)
+    return render_template("index.html", mars=data)
     
 
 
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-    data = scrape.scrape_info()
+
+    mars_data = scrape_mars.scrape()
     
     # use mongo update to upsert data
+    mongo.db.collection.update({}, mars_data, upsert=True)
     
     return redirect("/", code=302)
 
